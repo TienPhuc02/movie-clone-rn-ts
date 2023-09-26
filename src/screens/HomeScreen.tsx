5263ca7fe3feb7cb25cb5d2d1a0b8d6962246f99
Phuc Do Tien <108790360+TienPhuc02@users.noreply.github.com>
2023-09-26 19:58:20 +0700

    Movie Screen UI Part 1

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,13 +1,30 @@
+import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { SafeAreaView, Text, View } from "react-native";
-
+import { SafeAreaView, Text, TouchableOpacity, View } from "react-native";
+import {
+  Bars3CenterLeftIcon,
+  MagnifyingGlassIcon,
+} from "react-native-heroicons/outline";
+import { styles } from "../theme";
 const HomeScreen = () => {
   return (
-    <View>
+    <View className="bg-neutral-800 h-full pt-6">
       <SafeAreaView>
-        <Text className="mt-[20px] bg-neutral-800 h-full text-white">
-          Home Screen
-        </Text>
+        <StatusBar style="light" />
+        <View className="flex flex-row justify-between items-center mx-4">
+          <Bars3CenterLeftIcon size="30" strokeWidth={2} color="white" />
+          <Text className="text-white text-3xl font-bold">
+            <Text style={styles.text}>M</Text>
+            ovies
+          </Text>
+          <TouchableOpacity>
+            <MagnifyingGlassIcon
+              size="30"
+              strokeWidth={2}
+              color="white"
+            ></MagnifyingGlassIcon>
+          </TouchableOpacity>
+        </View>
       </SafeAreaView>
     </View>
   );
