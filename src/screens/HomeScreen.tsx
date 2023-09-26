60c70f34a797d62b5b1ea9b581eca11f9840013c
Phuc Do Tien <108790360+TienPhuc02@users.noreply.github.com>
2023-09-26 19:34:30 +0700

    first commit

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { SafeAreaView, Text, View } from "react-native";
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <SafeAreaView>
+        <Text className="mt-[20px] bg-neutral-800 h-full text-white">
+          Home Screen
+        </Text>
+      </SafeAreaView>
+    </View>
+  );
+};
+
+export default HomeScreen;
